49702da5013390fae060371533c146f2a139b6bd
Sparky <59151395+Sparks29032@users.noreply.github.com>
2022-03-12 18:09:33 -0600

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        // Board board = new Board();
-        // board.takeSS();
-        ScreenReader sr = new ScreenReader();
-        sr.takeSS();
-    }
-}
